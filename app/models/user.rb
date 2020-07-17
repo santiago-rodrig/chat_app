2847c9c4c5cd972fe43fe877c9676f9a2e6afddc
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-17 17:20:25 -0400

    fix: email regexp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ApplicationRecord
   has_many :messages, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates(
     :name,
     presence: true,
     uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 25 }
   )
+
   validates(
     :email,
     presence: true,
     uniqueness: { case_sensitive: false },
-    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid format' }
+    format: { with: VALID_EMAIL_REGEX, message: 'invalid format' }
   )
   has_secure_password
 end
+
